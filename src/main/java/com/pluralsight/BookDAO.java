77d3cc3a2795b8f6ee1db9d02aea7c8ed46418c2
sanderjk <43175560+sanderjk@users.noreply.github.com>
2018-09-11 21:14:46 +0200

    Completed the module2

--- src/main/java/com/pluralsight/BookDAO.java
+++ src/main/java/com/pluralsight/BookDAO.java
@@ -104,8 +104,8 @@ try{
     }
     public void updateBook(Book book) {
       String SQL = "UPDATE book SET title = ?, author = ?, price = ? " +
-       "WHERE id =?";
-try{
+                   "WHERE id = ?";
+    try{
       PreparedStatement statement = jdbcConnection.prepareStatement(SQL);
     }catch (SQLException e){
       e.printStackTrace();
