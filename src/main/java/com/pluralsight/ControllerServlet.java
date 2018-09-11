b6383fd037dd5aecd277010258812586193a784a
sanderjk <43175560+sanderjk@users.noreply.github.com>
2018-09-11 21:36:33 +0200

    Completed the module2

--- src/main/java/com/pluralsight/ControllerServlet.java
+++ src/main/java/com/pluralsight/ControllerServlet.java
@@ -65,6 +65,9 @@ public class ControllerServlet extends HttpServlet {
 				case "/edit":
 						showEditForm(request, response);
 						break;
+				case "/update":
+						updateBook(request, response);
+						break;
         default:
 				   listBooks(request, response);
            break;
@@ -138,4 +141,19 @@ throws ServletException, IOException{
 		doGet(request, response);
 
 	}
+
+private void updateBook(HttpServletRequest request, HttpServletResponse response)
+throws ServletException, IOException {
+	int id = Integer.parseInt(request.getParameter("id"));
+	String title = request.getParameter("booktitle");
+	String author = request.getParameter("author");
+	String price = request.getParameter("price");
+
+	Book newBook = new Book(id, title, author, Float.parseFloat(price));
+
+	bookDAO.updateBook(newBook);
+	response.sendRedirect("list");
+}
+
+
 }
